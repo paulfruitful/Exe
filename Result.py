70ffea63d4bedd9731ffcecf512142c77e64601e
Eze-og <152654164+Eze-og@users.noreply.github.com>
2023-12-08 21:14:03 +0100

    Add files via upload

---
+++ Result.py
@@ -0,0 +1,24 @@
+subjects =int(input("How Many Subjects? "))
+def calculate_average(scores):
+    total = sum(scores)
+    return total / len(scores)
+
+def main():
+    # create an empty list to store the scores
+    scores = []
+
+    for i in range(1, 8):
+        try:
+          score = float(input(f"Enter score for subject {i}: "))
+          scores.append(score)
+        except:
+            print("Invalid input. Please enter a valid numerical score.")
+        return
+
+    # Calculate and display the average score
+    average = calculate_average(scores)
+    print(f"The average score is: {average:.2f}")
+
+if __name__ == "__main__":
+    main()
+
