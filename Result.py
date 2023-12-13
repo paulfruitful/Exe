91c0bad38cec64182f578d3d3aa3c969174480dc
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-12-13 03:08:19 +0100

    Update Result.py

--- Result.py
+++ Result.py
@@ -6,15 +6,13 @@ def calculate_average(scores):
 def main():
     # create an empty list to store the scores
     scores = []
-
-    for i in range(1, 8):
+    for i in range(1, subjects+1):
         try:
           score = float(input(f"Enter score for subject {i}: "))
           scores.append(score)
         except:
             print("Invalid input. Please enter a valid numerical score.")
-        return
-
+        
     # Calculate and display the average score
     average = calculate_average(scores)
     print(f"The average score is: {average:.2f}")
